f047318bb03f64cb97017eb4e12f9dd1bc64ff15
Aleksandar Pajevic <aleksandar.pajevic@gmail.com>
2020-07-11 16:42:32 +0200

    click handlers, order button and priceing

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -3,22 +3,36 @@ import styles from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-  {label: 'Salad', type:'salad'},
-  {label: 'Bacon', type:'bacon'},
-  {label: 'Cheese', type:'cheese'},
-  {label: 'Meat', type:'meat'}
-]
-
-
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
 
 const buildControls = (props) => {
-  return(
+  return (
     <div className={styles.BuildControls}>
-      {controls.map(ctrl =>(        
-        <BuildControl key={ctrl.label} label={ctrl.label} />
+      <p>
+        Current Price: <strong>{props.price.toFixed(2)}</strong>$
+      </p>
+      {controls.map((ctrl) => (
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => {
+            props.ingredientAdded(ctrl.type);
+          }}
+          removed={() => {
+            props.ingredientRemoved(ctrl.type);
+          }}
+          disabled={props.disabledInfo[ctrl.type]}
+        />
       ))}
+      <button className={styles.OrderButton} disabled={props.purchasable}>
+        ORDER NOW
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default buildControls;
