e8e6880e14094b951c14e0b9e5dbfd9567245519
Aleksandar Pajevic <aleksandar.pajevic@gmail.com>
2020-07-14 22:39:37 +0200

    navigation

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './Toolbar.module.css';
 import Logo from '../Logo/Logo';
+import NavigationItems from '../Navigation/NavigationItems/NavigationItems';
 
 const toolbar = (props) => {
   return (
     <header className={styles.Toolbar}>
       <div>MENU</div>
       <Logo />
-      <nav>...</nav>
+      <NavigationItems />
     </header>
   );
 };
