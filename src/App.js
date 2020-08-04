f6e47f5249faffd7f5dabeaa42290a7923d4b80b
Aleksandar Pajevic <aleksandar.pajevic@gmail.com>
2020-08-04 18:28:30 +0200

    Orders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Orders from './containers/Orders/Orders';
 import Checkout from './containers/Checkout/Checkout';
 import {Route} from 'react-router-dom';
 
@@ -10,6 +11,7 @@ class App extends Component {
       <div>
         <Layout>
           <Route path='/' exact  component={BurgerBuilder} />
+          <Route path='/orders'  component={Orders} />
           <Route path='/checkout' component={Checkout} />
         </Layout>
       </div>
